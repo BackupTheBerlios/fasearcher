2ff9b7a62b2e44d7343d52b67d586dbcfb694498
pablof <pablof>
2008-01-17 17:31:55 +0000

    Creado un main principal

---
+++ ProgramadorGenetico/src/es/si/ProgramadorGenetico/MainPrincipal.java
@@ -0,0 +1,33 @@
+package es.si.ProgramadorGenetico;
+
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+import es.si.ProgramadorGenetico.Interfaz.DibujanteNuevo;
+import es.si.ProgramadorGenetico.Interfaz.FramePrincipal;
+import es.si.ProgramadorGenetico.Interfaz.PanelTablaTransiciones;
+import es.si.ProgramadorGenetico.ProblemaAFP.AFP;
+
+public class MainPrincipal {
+	
+	
+	private static void createAndShowGUI() {
+		FramePrincipal f = new FramePrincipal("Dibujante automatas");		
+		f.setLayout(new FlowLayout());
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
+        f.pack();
+        f.setVisible(true);
+        f.setExtendedState(Frame.MAXIMIZED_BOTH);
+	}
+	public static void main (String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+	}
+}
