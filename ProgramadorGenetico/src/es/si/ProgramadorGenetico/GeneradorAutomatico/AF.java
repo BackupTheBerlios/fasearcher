a4a93c72c762d0c0a75a6834afbfd314def1c85d
pablof <pablof>
2008-03-02 23:18:04 +0000

    Arreglado para generar el automata

--- ProgramadorGenetico/src/es/si/ProgramadorGenetico/GeneradorAutomatico/AF.java
+++ ProgramadorGenetico/src/es/si/ProgramadorGenetico/GeneradorAutomatico/AF.java
@@ -1,5 +1,8 @@
 package es.si.ProgramadorGenetico.GeneradorAutomatico;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class AF {
 	/**
 	 * Numero de estados
@@ -12,12 +15,51 @@ public class AF {
 	 */
 	private double[][][] transiciones;
 	
+	private double[] finales;
+	
 	public AF (int estados) {
 		this.estados = estados;
-		transiciones = new double[estados][2][estados + 1];
+		transiciones = new double[estados][2][estados];
+		finales = new double[estados];
 	}
 	
 	public void setTransicion (int origen, int valor, int destino) {
 		transiciones[origen][valor][destino]=1;
 	}
+	
+	public int getEstados () {
+		return estados;
+	}
+	
+	public double getTransicion (int origen, int valor, int destino) {
+		return transiciones[origen][valor][destino];
+	}
+	
+	public double [][][] getTransiciones () {
+		return transiciones;
+	}
+	
+	public double[] getFinales () {
+		return finales;
+	}
+	
+	public void setFinal (int estado) {
+		finales[estado] = 1;
+	}
+	
+	public String toString() {
+		String af = new String();
+		NumberFormat form = new DecimalFormat("0.0000");
+		
+		for (int i = 0; i < estados;i++) {
+			for (int j = 0; j < estados; j++) {
+				af += ""+i +":0:"+j+" >> " + form.format(transiciones[i][0][j]);
+				af += " || "+i +":1:"+j+" >> " + form.format(transiciones[i][1][j]) + "\n";
+
+			}
+		}
+
+		return af;
+	}
+
 }
--- ProgramadorGenetico/src/es/si/ProgramadorGenetico/GeneradorAutomatico/GeneradorAF.java
+++ ProgramadorGenetico/src/es/si/ProgramadorGenetico/GeneradorAutomatico/GeneradorAF.java
@@ -1,14 +1,80 @@
 package es.si.ProgramadorGenetico.GeneradorAutomatico;
 
+import java.util.ArrayList;
+
 public class GeneradorAF {
 
-	AF automata;
+	private class Pareja {
+		
+		int []trans;		
+		
+		public Pareja(int val0, int val1) {
+			trans = new int[2];
+			trans[0]=val0;
+			trans[1]=val1;
+		}
+		
+		public void setTrans (int valor) { 
+			trans[valor]=1;					
+		}
+		
+		public int getTrans (int valor) {
+			return trans[valor];
+		}
+	};
+	AF aut;
+	ArrayList<Pareja> transicionesExistentes;
 	
 	public GeneradorAF (int numEstados) {
-		automata = new AF(numEstados);			
+		aut = new AF(numEstados);		
 	}
 	
 	public void generarAutomata () {
-		for ()
+		transicionesExistentes = new ArrayList<Pareja>();
+		for (int i=0; i<aut.getEstados()-1; i++) {
+			int val = (int)(2*Math.random());
+			aut.setTransicion(i, val , i+1);
+			if (val==0)
+				transicionesExistentes.add(new Pareja(1,0));
+			else if (val==1)
+				transicionesExistentes.add(new Pareja(0,1));
+		}
+		
+		for (int i=0; i<aut.getEstados()-1; i++) {
+			if (transicionesExistentes.get(i).getTrans(0)==0) {
+				int estadoDestino = (int)(aut.getEstados()*Math.random());				
+				transicionesExistentes.get(i).setTrans(0);
+				aut.setTransicion(i,0,estadoDestino);
+				System.out.println("Aleatorio"+i+" "+estadoDestino);
+			}
+			if (transicionesExistentes.get(i).getTrans(1)==0) {
+				int estadoDestino = (int)(aut.getEstados()*Math.random());
+				transicionesExistentes.get(i).setTrans(1);
+				aut.setTransicion(i,1,estadoDestino);
+				System.out.println("Aleatorio"+i+" "+estadoDestino);
+			}									
+		}
+		
+		int estadoDestino = (int)(aut.getEstados()*Math.random());				
+		transicionesExistentes.add(new Pareja(1,0));
+		aut.setTransicion(aut.getEstados()-1,0,estadoDestino);
+		estadoDestino = (int)(aut.getEstados()*Math.random());				
+		transicionesExistentes.get(aut.getEstados()-1).setTrans(1);
+		aut.setTransicion(aut.getEstados()-1,1,estadoDestino);
+		
+		//Minimo un estado final
+		int estadoFinalSeguro = (int)(((aut.getEstados())*Math.random()));
+		aut.setFinal(estadoFinalSeguro);
+		for (int i=0; i<aut.getEstados(); i++) {
+			//Probabildad de que un estado sea final entre 0% y 40%
+			int probabilidadFinal = (int)(Math.random()*40);
+			int prob = (int)(100*(Math.random()));
+			if (prob >= probabilidadFinal)
+				aut.setFinal(i);							
+		}		
+	}
+	
+	public AF getAF () {
+		return aut;
 	}
 }
